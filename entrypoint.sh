42848a5f50b1bac6cafc5c57942412d73b6f43e1
Axel Haustant <noirbizarre@gmail.com>
2017-05-22 11:22:08 +0200

    Added more commands to entrypoint (celery, front, work, beat)

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,18 @@ case $1 in
         udata collect -ni /udata/public
         uwsgi --emperor /udata/uwsgi/
         ;;
+    front)
+        uwsgi /udata/uwsgi/front.ini
+        ;;
+    worker)
+        uwsgi /udata/uwsgi/worker.ini
+        ;;
+    beat)
+        uwsgi /udata/uwsgi/beat.ini
+        ;;
+    celery)
+        celery -A udata.worker "${@:2}"
+        ;;
     bash)
         /bin/bash
         ;;
