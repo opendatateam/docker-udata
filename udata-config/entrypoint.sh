44274adc29397cbd996ed330e4ca80e39630d39b
Julien Moura <julien.moura@oslandia.com>
2024-06-21 13:32:57 +0200

    déplace les fichiers de config dans un sous-dossier passé en volume

---
+++ udata-config/entrypoint.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e
+
+if [ "$(ls -A /src)" ]; then
+    ngstate="$(shopt -p nullglob || true)"
+    shopt -s nullglob
+
+    # Install source repositories as editable
+    for d in /src/*/ ; do
+        echo "Installing $d"
+        pip install -e "$d"
+    done
+    # Install packages from requirements files
+    for r in /src/*.pip ; do
+        echo "Installing dependencies from $r"
+        pip install -r "$r"
+    done
+    
+    $(eval $ngstate)
+
+fi
+
+case $1 in
+    uwsgi)
+        udata collect -ni /udata/public
+        uwsgi --emperor /udata/uwsgi/
+        ;;
+    front)
+        uwsgi /udata/uwsgi/front.ini
+        ;;
+    worker)
+        uwsgi /udata/uwsgi/worker.ini
+        ;;
+    beat)
+        uwsgi /udata/uwsgi/beat.ini
+        ;;
+    celery)
+        celery -A udata.worker "${@:2}"
+        ;;
+    bash)
+        /bin/bash
+        ;;
+    *)
+        udata "$@"
+        ;;
+esac
